8a27d7ae67a9c78a6f3c47f9b7828ef159d14216
Dima <dimasmatko3@gmail.com>
2025-05-10 11:56:07 +0300

    Setting up authorization

--- messenger/user/views.py
+++ messenger/user/views.py
@@ -1,6 +1,5 @@
 from django.views import View
 from django.shortcuts import render, redirect
-#from .models import UserModel
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
@@ -13,13 +12,15 @@ class RegistrationView(View):
     def post(self, request):
         name = request.POST.get('name')
         username = request.POST.get('username')
-        #surname = request.POST.get('surname')
         email = request.POST.get('email')
-        #gender = request.POST.get('gender')
-        #age = request.POST.get('age')
         password = request.POST.get('password')
 
-        User.objects.create_user(username = username, email=email, password= password)
+        if User.objects.filter(username=username).exists():
+            messages.error(request, 'Пользователь с таким логином уже существует.')
+            return redirect('reg')
+
+        User.objects.create_user(username=username, email=email, password=password)
+        messages.success(request, 'Вы успешно зарегистрированы! Войдите.')
         return redirect('auth')
 
 class AuthorizationView(View):
@@ -30,19 +31,14 @@ class AuthorizationView(View):
     def post(self, request):
         username = request.POST.get('username')
         password = request.POST.get('password')
-
-        # Проверка на пустые поля
         if not username or not password:
             messages.error(request, 'Введите логин и пароль!')
             return redirect('auth')
-
-        # Пытаемся аутентифицировать пользователя
         user = authenticate(request, username=username, password=password)
-        
         if user is not None:
             login(request, user)
             messages.success(request, 'Вы успешно вошли в систему!')
-            return redirect('/')  # Редирект на главную страницу или другую
+            return redirect('home')
         else:
             messages.error(request, 'Неверный логин или пароль.')
-            return redirect('auth')  # Редирект обратно на страницу авторизации
+            return redirect('auth')
